ba1b4b817b626bc257579a6dc9b5d0de7b0a8f85
Matt <spc.eubanks.matthew@gmail.com>
2018-05-06 08:50:54 -0700

    Update

---
+++ index.js
@@ -0,0 +1,10 @@
+function getDog() {
+    $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
+        // $(".breeds-image-random pre").html(JSON.stringify(data, null, 4));
+        $(".image-content").html("<img src='" + data.message + "'>");
+    });
+};
+
+$('.getDogPic').on('click', function () {
+    getDog();
+});
