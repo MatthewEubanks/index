2e42dd376b8e097fbef7600b7cd67ba8d61adc6f
Matt <spc.eubanks.matthew@gmail.com>
2018-05-27 11:37:00 -0700

    Newer Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getDog() {
     $.getJSON("https://dog.ceo/api/breeds/image/random", function (data) {
-        // $(".breeds-image-random pre").html(JSON.stringify(data, null, 4));
+        
         $(".image-content").html("<img src='" + data.message + "'>");
     });
 };
